2b202ee3ff83f2204f0b7452401d8be66eeff005
jonathan343 <jonathangee09@gmail.com>
2026-03-21 19:11:38 -0400

    Initial Commit

---
+++ MosaicTests/MosaicTests.swift
@@ -0,0 +1,16 @@
+//
+//  MosaicTests.swift
+//  MosaicTests
+//
+//  Created by Jonathan Gaytan on 3/21/26.
+//
+
+import Testing
+
+struct MosaicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
